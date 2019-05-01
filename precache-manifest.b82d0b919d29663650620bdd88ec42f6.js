b4cbcca24c814b770e1f4f46a9ac4ba25c924e21
manuelffc <37032534+manuelffc@users.noreply.github.com>
2019-05-01 16:01:17 -0600

    Updates

---
+++ precache-manifest.b82d0b919d29663650620bdd88ec42f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2e4622ab13bcb913b2584f0926c257",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "4fb3549fb690182435e4",
+    "url": "/react_app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react_app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4fb3549fb690182435e4",
+    "url": "/react_app/static/js/main.3258f42c.chunk.js"
+  },
+  {
+    "revision": "dd7a70cdeda87e112e79",
+    "url": "/react_app/static/js/runtime~main.cba007e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
